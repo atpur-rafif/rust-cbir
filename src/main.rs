b0af0ba9208c8a522d668a0a789f6f8cfcc6dd0e
atpur-rafif <muhammadatpurrafif@gmail.com>
2023-12-20 14:37:55 +0700

    Average cosine similarity for vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+struct Vectors {
+    size: usize,
+    dimension: usize,
+    buffer: Vec<f64>,
+}
+
+impl Vectors {
+    fn new(size: usize, dimension: usize) -> Vectors {
+        Vectors {
+            size,
+            dimension,
+            buffer: vec![0.0; size * dimension],
+        }
+    }
+
+    fn get_average_cosine_similarity(a: &Vectors, b: &Vectors) -> f64 {
+        let size = a.size;
+        let dimension = a.dimension;
+        if size != b.size || dimension != b.dimension {
+            panic!("Can't operate on different size or dimension of vectors");
+        }
+
+        let mut res = 0.0;
+        for i in 0..size {
+            let mut sum = 0.0;
+            for j in 0..dimension {
+                let idx = i * dimension + j;
+                let va = a.buffer[idx];
+                let vb = b.buffer[idx];
+                let diff = va - vb;
+                sum += diff * diff;
+            }
+            res += f64::sqrt(sum);
+        }
+        res /= size as f64;
+
+        res
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let img = image::open("./dataset/0.jpg").unwrap();
+    // println!("{} {}", img.width(), img.height());
+    let a = Vectors::new(3, 10);
+    let b = Vectors::new(3, 10);
+    dbg!(Vectors::get_average_cosine_similarity(&a, &b));
 }
