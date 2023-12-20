38210c07a676be1f7ffb85b7e67b5e894a5a73e6
Muhammad Atpur Rafif <muhammadatpurrafif@gmail.com>
2023-12-20 21:35:07 +0700

    Implemented custom thread pool

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,30 @@ mod pool;
 mod vectors;
 
 use std::{
+    fs,
     thread::{self, JoinHandle},
     time::{Duration, Instant},
     vec,
 };
 
 use image::GenericImageView;
+use pool::thread_pool;
 use rayon::prelude::*;
 
+use crate::vectors::Vectors;
+
 fn main() {
-    let all_timer = Instant::now();
-
-    let dir = std::fs::read_dir("./dataset").unwrap();
-    let mut threads = vec![] as Vec<JoinHandle<()>>;
-
-    let res = dir
-        .map(|file| file.unwrap().path().display().to_string())
-        .collect::<Vec<String>>()
-        .par_iter()
-        .map(|img_path| {
-            let now = Instant::now();
-
-            let img = image::open(&img_path).unwrap();
-            let res = vectors::Vectors::get_hsv_feature_vector(&img);
-
-            let elapsed = now.elapsed();
-            // println!("Processed Image in: {:.2?} {}", now.elapsed(), img_path);
-            ()
-        })
-        .collect::<Vec<()>>();
-
-    println!("Total time elapsed: {:.2?}", all_timer.elapsed());
+    let timer = Instant::now();
+    let mut a = vec![1, 2, 3];
+    let k = thread_pool(
+        fs::read_dir("./dataset")
+            .unwrap()
+            .map(|f| f.unwrap().path().display().to_string())
+            .collect::<Vec<String>>(),
+        |x| {
+            let img = image::open(&x).unwrap();
+            Vectors::get_hsv_feature_vector(&img)
+        },
+    );
+    println!("{:.2?}", timer.elapsed());
 }
---
+++ src/pool.rs
@@ -0,0 +1,50 @@
+use std::fmt::Debug;
+use std::marker::Send;
+use std::sync::mpsc::{self, Sender, TryRecvError};
+use std::thread;
+
+use rayon::iter::Empty;
+
+pub fn thread_pool<T: Send + 'static, U: Send + 'static>(
+    mut values: Vec<T>,
+    functor: fn(T) -> U,
+) -> Vec<U> {
+    let mut max_thread: usize = thread::available_parallelism().unwrap().into();
+    let size = values.len();
+    let (tx, rx) = mpsc::channel::<U>();
+
+    fn inner_spawn<T: Send + 'static, U: Send + 'static>(
+        value: T,
+        functor: fn(T) -> U,
+        transmitter: Sender<U>,
+    ) {
+        thread::spawn(move || {
+            transmitter.send(functor(value)).unwrap();
+        });
+    }
+
+    let mut res = Vec::new() as Vec<U>;
+
+    let mut started = 0;
+    while started < max_thread && started < size {
+        inner_spawn(values.pop().unwrap(), functor, tx.clone());
+        started += 1;
+    }
+
+    let mut finished = 0;
+    while finished < size {
+        match rx.try_recv() {
+            Ok(v) => {
+                res.push(v);
+                match values.pop() {
+                    Some(v) => inner_spawn(v, functor, tx.clone()),
+                    None => {}
+                }
+                finished += 1;
+            }
+            Err(e) => {}
+        }
+    }
+
+    res
+}
